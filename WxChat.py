97d9d1e579d7ec0ea8f55f44d4fbd5644681480c
Oiuv <i@oiuv.cn>
2023-05-29 10:17:41 +0800

    🧡💛💚💙💜🤎🖤

--- WxChat.py
+++ WxChat.py
@@ -2,17 +2,9 @@ import json
 import os
 import subprocess
 import time
-from functools import partial
-from multiprocessing import Pool
 import pandas as pd
 from tqdm import tqdm
 from decrypt import decrypt_data
-
-
-def process_data(cmd, decrypt_random_key, encrypt_chat_msg, i):
-    # 将decrypt_random_key和encrypt_chat_msg列中的数据依次传递给指令
-    subprocess.run(cmd + [decrypt_random_key[i], encrypt_chat_msg[i]],
-                   stdout=subprocess.PIPE)
 
 
 class WxChat:
@@ -104,32 +96,37 @@ class WxChat:
         end_time = time.time()
         print(f"数据预处理耗时 {end_time - start_time:.2f} 秒")
 
-def chat_data(file_name, sdktools_path='./sdktools'):
-    # 读取Excel文件
-    print(f"开始加载文件{file_name}……💕")
-    df = pd.read_excel(file_name, engine='openpyxl')
+    def chat_data(self, file_name):
+        # 统计代码运行时间
+        start_time = time.time()
 
-    # 获取decrypt_random_key和encrypt_chat_msg列的数据
-    decrypt_random_key = df['decrypt_random_key'].tolist()
-    encrypt_chat_msg = df['encrypt_chat_msg'].tolist()
+        # 读取Excel文件
+        print(f"开始加载文件{file_name}......💕")
+        df = pd.read_excel(file_name, engine='openpyxl')
 
-    # 构造指令
-    cmd = [sdktools_path, '3']
+        # 获取decrypt_random_key和encrypt_chat_msg列的数据
+        decrypt_random_key = df['decrypt_random_key'].tolist()
+        encrypt_chat_msg = df['encrypt_chat_msg'].tolist()
 
-    # 使用多进程加速数据处理过程
-    print("开始解密聊天记录……💕")
-    with Pool() as p:
-        func = partial(process_data, cmd, decrypt_random_key, encrypt_chat_msg)
-        for _ in tqdm(p.imap_unordered(func, range(len(decrypt_random_key))),
-                        total=len(decrypt_random_key), desc='Processing'):
-            pass
+        # 构造指令
+        cmd = [self.sdktools_path, '3']
 
-    # 打印结果
-    print('数据解密完成 ✔')
+        # 解密聊天记录
+        print("开始解密聊天记录......💕")
+        for i in tqdm(range(len(decrypt_random_key)), desc='Processing'):
+            subprocess.run(cmd + [decrypt_random_key[i], encrypt_chat_msg[i]],
+                           stdout=subprocess.PIPE)
+
+        # 打印结果
+        print('数据解密完成 ✔')
+
+        # 输出代码运行时间
+        end_time = time.time()
+        print(f"解密耗时 {end_time - start_time:.2f} 秒")
 
 
 if __name__ == '__main__':
     wx_chat = WxChat()
-    # wx_chat.get_data()
-    # wx_chat.data_to_excel('chat.jsonl')
-    chat_data('chat.xlsx')
+    wx_chat.get_data()
+    wx_chat.data_to_excel('chat.jsonl')
+    # wx_chat.chat_data('chat.xlsx')
