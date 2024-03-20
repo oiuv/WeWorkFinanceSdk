aa86bd99e4805315e9c6276629dc232cb32d34bc
Oiuv <i@oiuv.cn>
2024-03-20 15:46:09 +0800

    优化代码

--- rename2Md5.py
+++ rename2Md5.py
@@ -14,6 +14,7 @@ def rename_files_with_md5(directory):
     total_processed = 0
     renamed_files = 0
     deleted_files = 0
+    skipped_files = 0
     for root, dirs, files in os.walk(directory):
         for filename in files:
             file_path = os.path.join(root, filename)
@@ -25,6 +26,11 @@ def rename_files_with_md5(directory):
                 _, file_extension = os.path.splitext(filename)
                 # 构造新文件名（MD5哈希值 + 扩展名）
                 new_filename = md5_hash + file_extension
+                # 检查新文件名是否与原始文件名相同
+                if new_filename == filename:
+                    skipped_files += 1
+                    print(f"Skipped file with the MD5 name: {file_path}")
+                    continue
                 # 新文件路径
                 new_file_path = os.path.join(root, new_filename)
                 # 如果新文件已存在，则删除原始文件
@@ -38,9 +44,10 @@ def rename_files_with_md5(directory):
                     renamed_files += 1
                     print(f"Renamed file: {file_path} -> {new_file_path}")
     # 输出处理结果
-    print(f"Total files processed: {total_processed}")  
-    print(f"Files renamed: {renamed_files}")  
-    print(f"Duplicate files deleted: {deleted_files}")  
+    print(f"Total files processed: {total_processed}")
+    print(f"Files renamed: {renamed_files}")
+    print(f"Files skipped: {skipped_files}")
+    print(f"Duplicate files deleted: {deleted_files}")
 
 # 指定要处理的目录，包括file、image、voice、video，不包括call
 directory = 'data/file'
